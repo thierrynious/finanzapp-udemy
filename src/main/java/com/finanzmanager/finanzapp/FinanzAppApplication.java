a04f872d9cd4a3e4bc59a1f031b83ca2ca32891a
Thierrynious <thierrynious@hotmail.fr>
2026-01-25 19:38:45 +0100

    Section 02: Spring Boot Basics – Initial project setup

---
+++ src/main/java/com/finanzmanager/finanzapp/FinanzAppApplication.java
@@ -0,0 +1,13 @@
+package com.finanzmanager.finanzapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanzAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanzmanager/finanzapp/FinanzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanzmanager.finanzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
